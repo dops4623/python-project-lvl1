ea1c11a3fe96f4e2d3daae127d9ecb19982c41c8
dops4623 <94477710+dops4623@users.noreply.github.com>
2022-02-09 21:56:53 +0200

    Create brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import prompt
+
+from brain_games.cli import welcome_user
+from brain_games.games.calc import play_calc
+
+
+def play():
+    name = welcome_user()
+    play_calc(name)
+
+
+def main():
+    play()
+
+
+if __name__ == '__main__':
+    main()
