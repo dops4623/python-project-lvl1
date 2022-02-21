6d84bec8f554d8b992308f2bff8b47f19faf1442
Tatiana Smirnova <t.s.smirnova@gmail.com>
2022-02-21 18:47:10 +0200

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -12,11 +12,11 @@ def ask_question():
     n = random.randint(5, 9)
     n1 = random.randint(0, 10)
     add = random.randint(1, 10)
-    position = random.randint(2, n-1)
+    position = random.randint(2, n - 1)
     res = str(n1)
     prev = n1
     for i in range(n):
-        res += f' {prev+add}' if i+1 != position else ' ..'
+        res += f' {prev+add}' if i + 1 != position else ' ..'
         prev += add
     return res
 
@@ -24,9 +24,9 @@ def ask_question():
 def correct_answer(expression):
     a = expression.split(' ')
     if a[0] != '..' and a[1] != '..':
-        add = int(a[1])-int(a[0])
+        add = int(a[1]) - int(a[0])
     else:
-        add = int(a[3])-int(a[4])
+        add = int(a[3]) - int(a[4])
     i = a.index('..')
-    res = int(a[i-1])+add
+    res = int(a[i-1]) + add
     return res
